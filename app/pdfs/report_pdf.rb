edcb2ea3847a040b2d607697115ff273d9bfab49
Troy Leach <troyleach29@gmail.com>
2015-03-15 15:57:45 -0500

    got the each command to work

--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -6,6 +6,7 @@ class ReportPdf < Prawn::Document
     header
     text_content
     # table_content
+    name = Resume.find(1)
     
   end
 
@@ -17,14 +18,20 @@ class ReportPdf < Prawn::Document
   end
  
   def text_content
-
+    
     # The cursor for inserting content starts on the top left of the page. Here we move it down a little to create more space between the text and the image inserted above
     y_position = cursor - 50
  
     # The bounding_box takes the x and y coordinates for positioning its content and some options to style it
     bounding_box([0, y_position], :width => 270, :height => 300) do
-      text "blah", size: 15, style: :bold
-      text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum semper placerat. Aenean mattis fringilla risus ut fermentum. Fusce posuere dictum venenatis. Aliquam id tincidunt ante, eu pretium eros. Sed eget risus a nisl aliquet scelerisque sit amet id nisi. Praesent porta molestie ipsum, ac commodo erat hendrerit nec. Nullam interdum ipsum a quam euismod, at consequat libero bibendum. Nam at nulla fermentum, congue lectus ut, pulvinar nisl. Curabitur consectetur quis libero id laoreet. Fusce dictum metus et orci pretium, vel imperdiet est viverra. Morbi vitae libero in tortor mattis commodo. Ut sodales libero erat, at gravida enim rhoncus ut."
+      @wood.each do |wood|
+        text wood.species, size: 15, style: :bold
+      end
+
+      @wood.each do |wood|
+        text wood.dimensions, size: 15, style: :bold
+      end
+      
     end
  
     bounding_box([300, y_position], :width => 270, :height => 300) do
