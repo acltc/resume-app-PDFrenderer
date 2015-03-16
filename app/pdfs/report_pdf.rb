bcae5838c08bde982e724de4f38f9b0433fb549b
Raquel Rodriguez <rodriguez.reads@gmail.com>
2015-03-16 10:55:22 -0500

    styling resume

--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -17,10 +17,11 @@ class ReportPdf < Prawn::Document
     #This inserts an image in the pdf file and sets the size of the image
     # image "#{Rails.root}/app/assets/images/Stacked_wood.JPG", width: 150, height: 150
     #On it's own, this doesn't position anything in header
-    y_position = cursor - 100
+    #y_position = cursor - 100
      
-    text "#{@student.first_name} #{@student.last_name}", size: 30, style: :bold 
-    text "#{@student.email} #{@student.phone_number} #{@student.blog}", size: 12 
+    text "#{@student.full_name}", size: 30, style: :bold 
+    text "#{@student.email} #{@student.phone_number} #{@student.blog} #{@student.github} #{@student.linkedin}" , size: 10 
+    stroke_horizontal_rule
     
 
   end
@@ -28,22 +29,49 @@ class ReportPdf < Prawn::Document
   def text_content
     
     # The cursor for inserting content starts on the top left of the page. Here we move it down a little to create more space between the text and the image inserted above
-    y_position = cursor - 50
- 
+    y_position = cursor - 30
+    gap = 10
     # The bounding_box takes the x and y coordinates for positioning its content and some options to style it
-    bounding_box([0, y_position], :width => 270, :height => 300) do
-      text "Experience", size: 10, style: :bold 
-      text "#{@student.experiences}"
-      text "#{@student.educations}" 
-      text @student["job_title"]
-      p @student
-    
-
+    bounding_box([0, y_position], :width => 512, :height => 144) do
+      transparent(0.5) { stroke_bounds }
+      text "SKILLS", size: 12, style: :bold
+      
+      # bounding_box([0, cursor - gap], :width => 490, :height => 100) do
+        font_size 10
+        pad_top(10){
+      text "Pad_top with text. No nested bounding box. Duis vel tortor elementum, ultrices tortor vel, accumsan dui. Nullam in dolor rutrum, gravida turpis eu, vestibulum lectus. Pellentesque aliquet dignissim justo ut fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut venenatis massa non eros venenatis aliquet. Suspendisse potenti. Mauris sed tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Mauris sed tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per himenaeosAliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per himenaeos." }
+      # end
+      
     end
- 
-    bounding_box([300, y_position], :width => 270, :height => 300) do
-      text "Duis vel", size: 15, style: :bold
-      text "Duis vel tortor elementum, ultrices tortor vel, accumsan dui. Nullam in dolor rutrum, gravida turpis eu, vestibulum lectus. Pellentesque aliquet dignissim justo ut fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut venenatis massa non eros venenatis aliquet. Suspendisse potenti. Mauris sed tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+
+    y_position = cursor - 30
+    gap = 10
+    bounding_box([0, y_position], :width => 512, :height => 288) do
+      transparent(0.5) { stroke_bounds }
+
+      text "EXPERIENCE", size: 12, style: :bold 
+      pad_top(10) {
+      text "Job Title" 
+      text "Company Name" "start-end" }
+
+      bounding_box([gap, cursor - gap], :width => 490) do
+        font_size 10
+         text "Nested bounding box Duis vel tortor elementum, ultrices tortor vel, accumsan dui. Nullam in dolor rutrum, gravida turpis eu, vestibulum lectus. Pellentesque aliquet dignissim justo ut fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut venenatis massa non eros venenatis aliquet. Suspendisse potenti. Mauris sed tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+      end
+      
+    end
+
+    y_position = cursor - 30
+    gap = 10
+    bounding_box([0, y_position], :width => 512, :height => 108) do
+      transparent(0.5) { stroke_bounds }
+      text "EDUCATION", size: 12, style: :bold
+      text "University name"
+
+      bounding_box([gap, cursor - gap], :width => 490, :height => 90) do
+        font_size 10
+      text "Duis vel tortor elementum, ultrices tortor vel, accumsan dui. Nullam in dolor rutrum, gravida turpis eu, vestibulum lectus. Pellentesque aliquet dignissim justo ut fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut venenatis massa non eros venenatis aliquet. Suspendisse potenti. Mauris sed tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor."
+      end
     end
  
   end
