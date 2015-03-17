61c1abffe5f4d53ebd44dafdde2d1a2fcf4babe9
Troy Leach <troyleach29@gmail.com>
2015-03-16 20:13:33 -0500

    Made the pdf display in the browers

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,37 +1,18 @@
 class ResumesController < ApplicationController
 
-  # def index
-
-  #   @student = Resume.find("1")
-
-
-  #       respond_to do |format|
-  #         format.html
-  #         # format.json { render json: @wood }
-  #         format.pdf do
-  #           pdf = ReportPdf.new(@student)
-  #           send_data pdf.render, filename: 'report.pdf', type: 'application/pdf'
-  #         end
-
-  #       end
-
+  def index
       
-  # end
+  end
 
   def show
     @student = Resume.find(params[:id])
-
-
-        respond_to do |format|
-          format.html
-          # format.json { render json: @wood }
-          format.pdf do
-            pdf = ReportPdf.new(@student)
-            send_data pdf.render, filename: 'report.pdf', type: 'application/pdf'
-          end
-
-        end
-
+    respond_to do |format|
+      format.html
+      format.pdf do
+        pdf = ReportPdf.new(@student)
+        send_data pdf.render, type: 'application/pdf', disposition: "inline"
+      end
+    end
   end
 
 end
