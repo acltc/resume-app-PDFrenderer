9d0b9c5cdbb173d533dbc9765e23c584ff5c9f9a
Raquel Rodriguez <rodriguez.reads@gmail.com>
2015-03-16 20:32:12 -0500

    styling-boxes-display-pdf-in-browser

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -4,7 +4,6 @@ class ResumesController < ApplicationController
 
   #   @student = Resume.find("1")
 
-
   #       respond_to do |format|
   #         format.html
   #         # format.json { render json: @wood }
@@ -17,20 +16,29 @@ class ResumesController < ApplicationController
 
       
   # end
+  def index
+  end
 
   def show
     @student = Resume.find(params[:id])
-
-
         respond_to do |format|
-          format.html
-          # format.json { render json: @wood }
-          format.pdf do
-            pdf = ReportPdf.new(@student)
-            send_data pdf.render, filename: 'report.pdf', type: 'application/pdf'
-          end
+              format.html
+              format.pdf do
+                pdf = ReportPdf.new(@student)
+                send_data pdf.render, type: 'application/pdf', disposition: "inline"
+              end
+           end
 
-        end
+
+        # respond_to do |format|
+        #   format.html
+        #   # format.json { render json: @wood }
+        #   format.pdf do
+        #     pdf = ReportPdf.new(@student)
+        #     send_data pdf.render, filename: 'report.pdf', type: 'application/pdf'
+        #   end
+
+        # end
 
   end
 
--- app/pdfs/report_pdf.rb
+++ app/pdfs/report_pdf.rb
@@ -19,8 +19,9 @@ class ReportPdf < Prawn::Document
     #On it's own, this doesn't position anything in header
     #y_position = cursor - 100
      
-    text "#{@student.full_name}", size: 30, style: :bold 
+    text "#{@student.full_name}", size: 30, style: :bold, color: "#A7C8DB" 
     text "#{@student.email} #{@student.phone_number} #{@student.blog} #{@student.github} #{@student.linkedin}" , size: 10 
+    stroke_color "#A7C8DB"
     stroke_horizontal_rule
     
 
@@ -32,36 +33,41 @@ class ReportPdf < Prawn::Document
     y_position = cursor - 30
     gap = 10
     # The bounding_box takes the x and y coordinates for positioning its content and some options to style it
-    bounding_box([0, y_position], :width => 512, :height => 144) do
+    bounding_box([0, y_position], :width => 512) do
       # transparent(0.5) { stroke_bounds }
+      
       text "SKILLS", size: 12, style: :bold
       
         font_size 10
         pad_top(5) {
           @student.skills.each do |x|
-            text "#{x["skill_name"]}"
+            text "\u2022 #{x["skill_name"]}"
           end
           }
+        text "\u2022 Ruby on Rails"
+        text "\u2022 REST"
     end
 
     y_position = cursor - 30
     # gap = 10
-    bounding_box([0, y_position], :width => 512, :height => 288) do
+    bounding_box([0, y_position], :width => 512) do
       # transparent(0.5) { stroke_bounds }
-
-      text "EXPERIENCE", size: 12, style: :bold 
-      
+      text "EXPERIENCE", size: 12, style: :bold     
         font_size 10      
         pad_top(5) {
           @student.experiences.each do |x|
-            text "#{x["job_title"]}" 
-            text "#{x["company_name"]}"  
+            text "#{x["job_title"].upcase}" 
+            text "#{x["company_name"].upcase}"  
           end 
           }
 
       bounding_box([gap, cursor - gap], :width => 490) do
         font_size 10
-         text "Nested bounding box Duis vel tortor elementum, ultrices tortor vel, accumsan dui. Nullam in dolor rutrum, gravida turpis eu, vestibulum lectus. Pellentesque aliquet dignissim justo ut fringilla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut venenatis massa non eros venenatis aliquet. Suspendisse potenti. Mauris sed tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+        text "Nested bounding box Duis vel tortor elementum
+                ultrices tortor vel, accumsan dui. Nullam in dolor rutrum, gravida turpis euvestibulum lectus. Pellentesque aliquet dignissim justo ut fringilla.
+                Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut venenatis massa non eros venenatis aliquet. Suspendisse potenti. Mauris sed 
+                tincidunt mauris, et vulputate risus. Aliquam eget nibh at erat dignissim aliquam non et risus. Fusce mattis neque id diam pulvinar, fermentum luctus enim porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+              
       end
       
     end
@@ -73,8 +79,8 @@ class ReportPdf < Prawn::Document
         font_size 10
         pad_top(5) {
         @student.educations.each do |x|
-          text "#{x["university_name"]}" 
-          text "#{x["degreed"]}"  
+          text "#{x["university_name"].upcase}" 
+          text "#{x["degreed"].upcase}"  
         end 
         }
 
